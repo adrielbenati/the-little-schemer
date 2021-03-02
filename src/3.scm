3315509f63a64ca2cd1689a1f585e94436385220
aabm <aabm@disroot.org>
2021-03-02 09:57:33 -0300

    Add `multiinsertR' function

--- src/3.scm
+++ src/3.scm
@@ -62,3 +62,13 @@
      (else
       (cons (car lat)
             (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     ((eq? (car lat) old)
+      (cons old (cons new (multiinsertR new old (cdr lat)))))
+     (else (cons (car lat)
+                 (multiinsertR new old (cdr lat)))))))
+
