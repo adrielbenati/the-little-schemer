849c3f5ba2cf142d4b759aa2dd9252020a968e9c
aabm <aabm@disroot.org>
2021-03-01 21:20:49 -0300

    Add `subst' function

--- src/3.scm
+++ src/3.scm
@@ -30,3 +30,13 @@
       (cons new lat))
      (else
       (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     ((eq? (car lat) old)
+      (cons new (cdr lat)))
+     (else
+      (cons (car lat)
+            (subst new old (cdr lat)))))))
