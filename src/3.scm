c926a33221da273f854c4793b4c518761504770f
aabm <aabm@disroot.org>
2021-03-01 21:53:45 -0300

    Add `subst2' function and explanation

--- src/3.scm
+++ src/3.scm
@@ -40,3 +40,16 @@
      (else
       (cons (car lat)
             (subst new old (cdr lat)))))))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+  (cond
+   ((null? lat) (quote ()))
+   ((eq? (car lat) o1)
+    (cons new (cdr lat)))
+   ((eq? (car lat) o2)
+    (cons new (cdr lat)))
+   (else
+    (cons (car lat)
+          (subst2 new o1 o2
+                  (cdr lat)))))))
