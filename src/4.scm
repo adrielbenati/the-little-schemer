ee925145b134266d12b1e2cf08986accc3f548c5
aabm <aabm@disroot.org>
2021-03-04 15:04:11 -0300

    Add `rempick' function

--- src/4.scm
+++ src/4.scm
@@ -112,3 +112,12 @@
       (pick (sub1 n)
             (cdr lat))))))
 
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n))
+      (cdr lat))
+     (else
+      (cons (car lat)
+            (rempick (sub1 n)
+                     (cdr lat)))))))
