c0b4588141ad8703696b74c283d4e7cbe720f3b2
aabm <aabm@disroot.org>
2021-03-04 14:06:21 -0300

    Add: `<' `>' and both versions of `='

--- src/4.scm
+++ src/4.scm
@@ -70,6 +70,13 @@
 (define =
   (lambda (n m)
     (cond
+     ((zero? n) (zero? m))
+     (else
+      (= (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond
      ((< n m) #f)
      ((> n m) #f)
      (else #t))))
