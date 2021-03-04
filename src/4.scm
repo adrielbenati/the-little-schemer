9949fff68d37d3a9108f6cd9aef43002ed1376f5
aabm <aabm@disroot.org>
2021-03-04 13:38:06 -0300

    Add `tup+' function

--- src/4.scm
+++ src/4.scm
@@ -41,3 +41,13 @@
      ((zero? m) 0)
      (else
       (plus n (x n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+     ((and (null? tup1)
+           (null? tup2))
+      (quote ()))
+     (else
+      (cons (+ (car tup1) (car tup2))
+            (tup+ (cdr tup1) (cdr tup2)))))))
