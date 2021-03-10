52a1c8fad4d7a20ea6175e3258de13ac2949c5b3
aabm <aabm@disroot.org>
2021-03-10 15:04:13 -0300

    Add `insertR*' function

--- src/5.scm
+++ src/5.scm
@@ -18,3 +18,18 @@
      ((eq? (car lat) a) 1)
      (else
       (add1 (position a (cdr lat)))))))
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons old (cons new (cdr l))))
+       (else
+        (cons (car l) (insertR* new old (cdr l))))))
+     (else
+      (cons (insertR* new old (car l))
+            (insertR* new old (cdr l)))))))
+
