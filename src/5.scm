3a51882d426af31e710ace6c0d660744c0090dda
aabm <aabm@disroot.org>
2021-03-10 15:10:47 -0300

    Add `insertL*' function

--- src/5.scm
+++ src/5.scm
@@ -66,3 +66,18 @@
       (cons (subst* new old (car l))
             (subst* new old (cdr l)))))))
 
+(define insertL*
+  (lambda (new old l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (insertL* new old (cdr l))))
+       (else
+        (cons (car l)
+              (insertL* new old (cdr l))))))
+     (else
+      (cons (insertL* new old (car l))
+            (insertL* new old (cdr l)))))))
